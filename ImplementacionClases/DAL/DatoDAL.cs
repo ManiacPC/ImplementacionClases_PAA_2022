95a05cd96f51f3106b3ab55e456bfb3c7a65d05c
Enrique Palacios <enrique.palacios@inacapmail.cl>
2022-05-16 16:10:51 -0400

    Agregar archivos de proyecto.

---
+++ ImplementacionClases/DAL/DatoDAL.cs
@@ -0,0 +1,29 @@
+﻿using ImplementacionClases.DTO;
+
+namespace ImplementacionClases.DAL
+{
+    public class DatoDAL
+    {
+        public bool Insertar(DatoDTO datos)
+        {
+            return DatoDTO.Add(datos);
+        }
+
+        public bool Actualizar(DatoDTO datos)
+        {
+            return false; // NO OLVIDAR
+        }
+
+        public bool Eliminar(DatoDTO datos)
+        {
+            return false; // NO OLVIDAR
+        }
+
+        public List<DatoDTO> Listar()
+        {
+            // return null;
+            return DatoDTO.List();
+        }
+
+    }
+}
