baf8732f8cdc4dc2ce73770bf77d5b0cd5d9efb1
Enrique Palacios <enrique.palacios@inacapmail.cl>
2022-05-17 13:01:52 -0400

    Avance en método actualizar - GUI, DAL y DTO

--- ImplementacionClases/DAL/DatoDAL.cs
+++ ImplementacionClases/DAL/DatoDAL.cs
@@ -9,9 +9,13 @@ namespace ImplementacionClases.DAL
             return DatoDTO.Add(datos);
         }
 
-        public bool Actualizar(DatoDTO datos)
+        public bool Actualizar(DatoDTO dato)
         {
-            return false; // NO OLVIDAR
+            // obtener índice de objeto a editar
+            int indice = BuscarPorId(dato); 
+            
+            // retornar el resultado de la edición del objeto en la lista
+            return DatoDTO.Edit(dato, indice);  
         }
 
         public bool Eliminar(DatoDTO datos)
@@ -23,6 +27,54 @@ namespace ImplementacionClases.DAL
         {
             // return null;
             return DatoDTO.List();
+        }
+
+        public int BuscarPorId(DatoDTO dato)
+        {
+            // método 1: tradicional (intro a programación)
+            for (int i = 0; i < DatoDTO.List().Count; i++)
+            {
+                // si encuentra elemento
+                if (DatoDTO.List()[i].Id == dato.Id)
+                {
+                    return i; // retorna índice de elemento
+                }
+            }
+            return 0;
+        }
+
+        // el "?" implica que el retorno puede ser nulo
+        public DatoDTO? BuscarPorId(int id)
+        {
+            //// método 1: tradicional (intro a programación)
+            //for(int i = 0; i < DatoDTO.List().Count; i++)
+            //{
+            //    // si encuentra elemento
+            //    if (DatoDTO.List()[i].Id == id)
+            //    {
+            //        return DatoDTO.List()[i]; // retorna elemento
+            //    } 
+            //}
+
+            //// retorno en caso de no encontrar
+            //return null; 
+
+            // método 2: foreach
+            foreach (DatoDTO dato in DatoDTO.List())
+            {
+                if (dato.Id == id) { return dato; }
+            }
+
+            // retorno en caso de no encontrar
+            return null;
+
+            //// método 3: expresiones (expresiones LinQ)
+            //// method syntax
+            //DatoDTO resultadoDato = DatoDTO.List().FirstOrDefault(x => x.Id == id);
+            //// query syntax
+            //DatoDTO resultadoDato2 = (from dato in DatoDTO.List()
+            //                         where dato.Id == id
+            //                         select dato).FirstOrDefault();
         }
 
     }
